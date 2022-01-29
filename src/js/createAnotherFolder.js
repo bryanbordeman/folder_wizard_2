830466ea82632fef15fb422ce5398629916a3913
Bryan Bordeman <bryanbordeman@Bryans-MBP.home>
2022-01-29 06:35:32 -0500

    added createAnotherFolder function

---
+++ src/js/createAnotherFolder.js
@@ -0,0 +1,11 @@
+function createAnotherFolder(){
+    $("#ConfirmationModal").modal('hide');
+    successLog.setAttribute("style","display:none;");
+    failLog.setAttribute("style","display:none;");
+    successReadme.setAttribute("style","display:none;");
+    failReadme.setAttribute("style","display:none;");
+    successFolder.setAttribute("style","display:none;");
+    failFolder.setAttribute("style","display:none;");
+    successRecord.setAttribute("style","display:none;");
+    failRecord.setAttribute("style","display:none;");
+}
